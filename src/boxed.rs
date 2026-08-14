78314b53f3b511c7a2a58a5fbf172e9f4b6821a9
Noam2Stein <noamsteindev@proton.me>
2026-08-14 18:03:38 +0300

    Add box

---
+++ src/boxed.rs
@@ -0,0 +1,130 @@
+use alloc::alloc::{alloc, handle_alloc_error};
+use core::{
+    alloc::Layout,
+    mem::ManuallyDrop,
+    ops::{Deref, DerefMut},
+    ptr::{NonNull, drop_in_place, read},
+};
+
+pub struct Box<T>(
+    /// # Safety
+    ///
+    /// This pointer must be convertable to a reference.
+    NonNull<T>,
+);
+
+impl<T> Box<T> {
+    pub fn new(value: T) -> Self {
+        let value = ManuallyDrop::new(value);
+
+        if size_of::<T>() == 0 {
+            Self(NonNull::dangling())
+        } else {
+            let layout = Layout::new::<T>();
+
+            // SAFETY: `layout` has a non-zero size
+            let ptr = unsafe { alloc(layout) };
+
+            let Some(ptr) = NonNull::new(ptr.cast::<T>()) else {
+                handle_alloc_error(layout);
+            };
+
+            // SAFETY: `ptr` is properly aligned and points to a valid
+            // allocation we just made.
+            unsafe { ptr.write(ManuallyDrop::into_inner(value)) };
+
+            // SAFETY: `ptr` points to a properly initialized value of `T`,
+            // which will not be dropped until `self` is dropped. The pointer
+            // is also properly aligned.
+            Self(ptr)
+        }
+    }
+
+    pub fn into_inner(boxed: Box<T>) -> T {
+        let boxed = ManuallyDrop::new(boxed);
+
+        // The pointer is guaranteed to be aligned, and we have exclusive access
+        // to the pointed at value.
+        unsafe { read(boxed.0.as_ptr()) }
+    }
+}
+
+impl<T> Deref for Box<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // SAFETY: `self.0` is guaranteed to be convertable to a pointer
+        unsafe { self.0.as_ref() }
+    }
+}
+
+impl<T> DerefMut for Box<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // SAFETY: `self.0` is guaranteed to be convertable to a pointer
+        unsafe { self.0.as_mut() }
+    }
+}
+
+impl<T> Drop for Box<T> {
+    fn drop(&mut self) {
+        // SAFETY: The pointer is convertable to a reference, we have exlusive
+        // access, and it will never be used again
+        unsafe { drop_in_place(self.0.as_ptr()) };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::boxed::Box;
+
+    const _: () = {
+        assert!(size_of::<Box<i32>>() == size_of::<usize>());
+        assert!(size_of::<Box<()>>() == size_of::<usize>());
+    };
+
+    #[test]
+    fn test_new() {
+        let _ = Box::new(5);
+    }
+
+    #[test]
+    fn test_into_inner() {
+        let boxed = Box::new(5);
+        assert_eq!(Box::into_inner(boxed), 5);
+    }
+
+    #[test]
+    fn test_deref() {
+        let boxed = Box::new(5);
+        assert_eq!(&*boxed, &5);
+    }
+
+    #[test]
+    fn test_deref_mut() {
+        let mut boxed = Box::new(5);
+        *boxed = 3;
+        assert_eq!(&*boxed, &3);
+    }
+
+    #[test]
+    fn test_drop() {
+        struct Ty<'a> {
+            drop_called: &'a mut bool,
+        }
+
+        impl<'a> Drop for Ty<'a> {
+            fn drop(&mut self) {
+                *self.drop_called = true;
+            }
+        }
+
+        let mut drop_called = false;
+        {
+            let _ = Box::new(Ty {
+                drop_called: &mut drop_called,
+            });
+        }
+
+        assert!(drop_called);
+    }
+}
